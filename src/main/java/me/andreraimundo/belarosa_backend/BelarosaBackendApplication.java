a85ea6d91e8b7bd78d99576ddf57620e8e900917
ANDRERAIMUNDO <9000andre@gmail.com>
2021-03-21 13:33:19 -0300

    Criando projeto

---
+++ src/main/java/me/andreraimundo/belarosa_backend/BelarosaBackendApplication.java
@@ -0,0 +1,13 @@
+package me.andreraimundo.belarosa_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelarosaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelarosaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/andreraimundo/belarosa_backend/BelarosaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.andreraimundo.belarosa_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelarosaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
