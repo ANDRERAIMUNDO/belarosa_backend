90b151913a76b9cf00a85806e13c239aec8a98bf
ANDRERAIMUNDO <9000andre@gmail.com>
2021-03-22 09:55:36 -0300

    Configunrando Banco H2 no projeto

--- src/main/java/me/andreraimundo/belarosa_backend/domain/Registro.java
+++ src/main/java/me/andreraimundo/belarosa_backend/domain/Registro.java
@@ -2,10 +2,18 @@ package me.andreraimundo.belarosa_backend.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Registro  implements Serializable{
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String email;
     private String password;
