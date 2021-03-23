4a1088474fce5425a258dec6f833c8c296f512e9
ANDRERAIMUNDO <9000andre@gmail.com>
2021-03-23 17:22:10 -0300

    #Adicionando exeção Objeto não econtrado em Endereço

--- src/main/java/me/andreraimundo/belarosa_backend/services/EnderecoService.java
+++ src/main/java/me/andreraimundo/belarosa_backend/services/EnderecoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import me.andreraimundo.belarosa_backend.domain.Endereco;
 import me.andreraimundo.belarosa_backend.repositories.EnderecoRepository;
+import me.andreraimundo.belarosa_backend.services.exception.ObjectNotFoundException;
 
 @Service
 public class EnderecoService {
@@ -16,7 +17,9 @@ public class EnderecoService {
     
     public Endereco find (Integer id){
         Optional <Endereco> obj = enderecoRepository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(()-> new 
+        ObjectNotFoundException("Objeto não encontrado Id: "+ id + " Tipo: "
+         + Endereco.class.getName()));
     }
     
 }
