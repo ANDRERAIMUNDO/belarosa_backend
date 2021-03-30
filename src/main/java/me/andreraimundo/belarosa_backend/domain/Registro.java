501df0dcbf39a948f97935723eb9b63d5492a443
ANDRERAIMUNDO <9000andre@gmail.com>
2021-03-30 19:40:17 -0300

    Criação system.properties e Procfile

--- src/main/java/me/andreraimundo/belarosa_backend/domain/Registro.java
+++ src/main/java/me/andreraimundo/belarosa_backend/domain/Registro.java
@@ -1,16 +1,12 @@
 package me.andreraimundo.belarosa_backend.domain;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
