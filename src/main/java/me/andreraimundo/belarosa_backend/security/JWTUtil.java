32bcc739b477181a5ea6b4be24fe81da24d31e71
ANDRERAIMUNDO <9000andre@gmail.com>
2021-05-03 15:01:39 -0300

    Correção de anotações- correção de erros

--- src/main/java/me/andreraimundo/belarosa_backend/security/JWTUtil.java
+++ src/main/java/me/andreraimundo/belarosa_backend/security/JWTUtil.java
@@ -3,11 +3,13 @@ package me.andreraimundo.belarosa_backend.security;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.Claims;
 
+@Component
 public class JWTUtil {
     @Value("${jwt.secret}")
 	private String secret;
