b311de96d8dfccc4d2f464df5633ac56df87d401
ANDRERAIMUNDO <9000andre@gmail.com>
2021-05-31 10:07:48 -0300

    Deploy 2

--- belarosa.sql
+++ belarosa.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.23, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.25, for Linux (x86_64)
 --
 -- Host: localhost    Database: belarosa
 -- ------------------------------------------------------
--- Server version	8.0.23
+-- Server version	8.0.25
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -24,9 +24,9 @@ DROP TABLE IF EXISTS `categoria`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categoria` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -48,12 +48,15 @@ DROP TABLE IF EXISTS `cliente`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cliente` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `cpf` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `date_nasc` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `phone` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `cpf` varchar(255) DEFAULT NULL,
+  `date_nasc` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `registro_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKbr0y8pbkpqhuhio7wybfyrsy6` (`registro_id`),
+  CONSTRAINT `FKbr0y8pbkpqhuhio7wybfyrsy6` FOREIGN KEY (`registro_id`) REFERENCES `registro` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,7 +65,7 @@ CREATE TABLE `cliente` (
 
 LOCK TABLES `cliente` WRITE;
 /*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
-INSERT INTO `cliente` VALUES (1,'630.792.070-09','13/12/1991','Lara Ramos','(91)9 88220467'),(2,'867.738.940-77','24/06/1998','Luana Lobato','(91)9 84001327'),(3,'291.672.410-93','10/03/1993','Pamela Lorena','(91)9 99001422');
+INSERT INTO `cliente` VALUES (1,'630.792.070-09','13/12/2009','Andre Raimundo','(91)9 88220467',1),(2,'867.738.940-77','24/06/1993','Luana Lobato','(91)9 84001327',2),(3,'291.672.410-93','10/03/1991','Pamela Lorena','(91)9 99001422',3);
 /*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -75,17 +78,21 @@ DROP TABLE IF EXISTS `endereco`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `endereco` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `bairro` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `cep` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `complemento` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `localidade` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `logradouro` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `uf` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bairro` varchar(255) DEFAULT NULL,
+  `cep` varchar(255) DEFAULT NULL,
+  `complemento` varchar(255) DEFAULT NULL,
+  `localidade` varchar(255) DEFAULT NULL,
+  `logradouro` varchar(255) DEFAULT NULL,
+  `numero` varchar(255) DEFAULT NULL,
+  `uf` varchar(255) DEFAULT NULL,
   `cliente_id` int DEFAULT NULL,
+  `registro_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK8s7ivtl4foyhrfam9xqom73n9` (`cliente_id`),
-  CONSTRAINT `FK8s7ivtl4foyhrfam9xqom73n9` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  KEY `FKrk4nvpy0exgj3tmqi78a75sm3` (`registro_id`),
+  CONSTRAINT `FK8s7ivtl4foyhrfam9xqom73n9` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`),
+  CONSTRAINT `FKrk4nvpy0exgj3tmqi78a75sm3` FOREIGN KEY (`registro_id`) REFERENCES `registro` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -94,7 +101,7 @@ CREATE TABLE `endereco` (
 
 LOCK TABLES `endereco` WRITE;
 /*!40000 ALTER TABLE `endereco` DISABLE KEYS */;
-INSERT INTO `endereco` VALUES (1,'Sacramenta','66123-120','368','Belém','Passagem Vila Nova','PA',1),(2,'Vila Nova','68655-000','38','Irituia','Rua Coronel Jose Vieira','PA',2),(3,'Vila Nova','68655-000','34','Irituia','Rua Bom Sossego','PA',3);
+INSERT INTO `endereco` VALUES (1,'Sacramenta','66123-120','Casa F','Belém','Passagem Vila Nova','368','PA',1,1),(2,'Vila Nova','68655-000','centro','Irituia','Rua Coronel Jose Vieira','38','PA',2,2),(3,'Vila Nova','68655-000','centro','Irituia','Rua Bom Sossego','34','PA',3,3);
 /*!40000 ALTER TABLE `endereco` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -115,7 +122,7 @@ CREATE TABLE `item_pedido` (
   KEY `FKtk55mn6d6bvl5h0no5uagi3sf` (`produto_id`),
   CONSTRAINT `FK60ym08cfoysa17wrn1swyiuda` FOREIGN KEY (`pedido_id`) REFERENCES `pedido` (`id`),
   CONSTRAINT `FKtk55mn6d6bvl5h0no5uagi3sf` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -140,7 +147,7 @@ CREATE TABLE `pagamento` (
   `situacao_pedido` int DEFAULT NULL,
   PRIMARY KEY (`pedido_id`),
   CONSTRAINT `FKthad9tkw4188hb3qo1lm5ueb0` FOREIGN KEY (`pedido_id`) REFERENCES `pedido` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -166,7 +173,7 @@ CREATE TABLE `pagamento_boleto` (
   `pedido_id` int NOT NULL,
   PRIMARY KEY (`pedido_id`),
   CONSTRAINT `FKdqk2a6n9it7vodk056daxe4jy` FOREIGN KEY (`pedido_id`) REFERENCES `pagamento` (`pedido_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -191,7 +198,7 @@ CREATE TABLE `pagamento_cartao` (
   `pedido_id` int NOT NULL,
   PRIMARY KEY (`pedido_id`),
   CONSTRAINT `FK936sknk6dwq1oa3p2y7d4s8ol` FOREIGN KEY (`pedido_id`) REFERENCES `pagamento` (`pedido_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -212,11 +219,11 @@ DROP TABLE IF EXISTS `pagamento_dinheiro`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `pagamento_dinheiro` (
-  `valor_recebido` double DEFAULT NULL,
+  `data_pagamento` datetime(6) DEFAULT NULL,
   `pedido_id` int NOT NULL,
   PRIMARY KEY (`pedido_id`),
   CONSTRAINT `FKb5ma55icc6n3rc2h9m7tg628e` FOREIGN KEY (`pedido_id`) REFERENCES `pagamento` (`pedido_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -225,7 +232,7 @@ CREATE TABLE `pagamento_dinheiro` (
 
 LOCK TABLES `pagamento_dinheiro` WRITE;
 /*!40000 ALTER TABLE `pagamento_dinheiro` DISABLE KEYS */;
-INSERT INTO `pagamento_dinheiro` VALUES (100,3);
+INSERT INTO `pagamento_dinheiro` VALUES ('2021-01-21 20:00:00.000000',3);
 /*!40000 ALTER TABLE `pagamento_dinheiro` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -241,12 +248,15 @@ CREATE TABLE `pedido` (
   `instante` datetime(6) DEFAULT NULL,
   `cliente_id` int DEFAULT NULL,
   `endereco_de_entrega_id` int DEFAULT NULL,
+  `registro_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK30s8j2ktpay6of18lbyqn3632` (`cliente_id`),
   KEY `FK1fihyy2fnocpuwc74674qmfkv` (`endereco_de_entrega_id`),
+  KEY `FKe8xqg6t8ctck3s3e00h408tlo` (`registro_id`),
   CONSTRAINT `FK1fihyy2fnocpuwc74674qmfkv` FOREIGN KEY (`endereco_de_entrega_id`) REFERENCES `endereco` (`id`),
-  CONSTRAINT `FK30s8j2ktpay6of18lbyqn3632` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  CONSTRAINT `FK30s8j2ktpay6of18lbyqn3632` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`),
+  CONSTRAINT `FKe8xqg6t8ctck3s3e00h408tlo` FOREIGN KEY (`registro_id`) REFERENCES `registro` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -255,8 +265,33 @@ CREATE TABLE `pedido` (
 
 LOCK TABLES `pedido` WRITE;
 /*!40000 ALTER TABLE `pedido` DISABLE KEYS */;
-INSERT INTO `pedido` VALUES (1,'2021-01-23 22:00:00.000000',1,1),(2,'2021-01-22 21:00:00.000000',2,2),(3,'2021-01-21 20:00:00.000000',3,3);
+INSERT INTO `pedido` VALUES (1,'2021-01-23 22:00:00.000000',1,1,1),(2,'2021-01-22 21:00:00.000000',2,2,2),(3,'2021-01-21 20:00:00.000000',3,3,3);
 /*!40000 ALTER TABLE `pedido` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `perfis`
+--
+
+DROP TABLE IF EXISTS `perfis`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `perfis` (
+  `registro_id` int NOT NULL,
+  `perfis` int DEFAULT NULL,
+  KEY `FKj7j0wuhtl2k4xwpk3t77ylc3` (`registro_id`),
+  CONSTRAINT `FKj7j0wuhtl2k4xwpk3t77ylc3` FOREIGN KEY (`registro_id`) REFERENCES `registro` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `perfis`
+--
+
+LOCK TABLES `perfis` WRITE;
+/*!40000 ALTER TABLE `perfis` DISABLE KEYS */;
+INSERT INTO `perfis` VALUES (1,1),(2,1),(3,1),(3,2);
+/*!40000 ALTER TABLE `perfis` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -268,10 +303,10 @@ DROP TABLE IF EXISTS `produto`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `produto` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
   `price` double DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -280,7 +315,7 @@ CREATE TABLE `produto` (
 
 LOCK TABLES `produto` WRITE;
 /*!40000 ALTER TABLE `produto` DISABLE KEYS */;
-INSERT INTO `produto` VALUES (1,'CARTEIRA VICTORINE',279.99),(2,'Bolsa Santa Lolla Baú Marrom',279.99),(3,'LES 4 OMBRES QUARTETO DE SOMBRAS MULTIEFEITOS',199.9),(4,'Bolsa Feminina Grande Tote Transversal Couro Ecológico',89.65);
+INSERT INTO `produto` VALUES (1,'CARTEIRA VICTORINE',279.99),(2,'Bolsa Santa Lolla Baú Marrom',279.99),(3,'LES 4 OMBRES QUARTETO DE SOMBRAS MULTIEFEITOS',199.9),(4,'Bolsa Feminina Grande Tote Transversal Couro Ecológico',89.65),(5,'Bolsa + Sapato Feminino Coleção Verão',99.99),(6,'Produto 1',99.99),(7,'Produto 2',99.99),(8,'Produto 3',99.99),(9,'Produto 4',99.99),(10,'Produto 5',99.99),(11,'Produto 6',99.99),(12,'Produto 7',99.99),(13,'Produto 8',99.99),(14,'Produto 9',99.99),(15,'Produto 10',99.99),(16,'Produto 11',99.99),(17,'Produto 12',99.99),(18,'Produto 13',99.99),(19,'Produto 14',99.99),(20,'Produto 15',99.99),(21,'Produto 16',99.99),(22,'Produto 17',99.99),(23,'Produto 18',99.99),(24,'Produto 19',99.99),(25,'Produto 20',99.99),(26,'Produto 21',99.99),(27,'Fantazia Noturna Dracula',129.9),(28,'Rabo de gato',9.9),(29,'Tapa olho do pirata',7);
 /*!40000 ALTER TABLE `produto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -298,7 +333,7 @@ CREATE TABLE `produto_categoria` (
   KEY `FK1c0y58d3n6x3m6euv2j3h64vt` (`produto_id`),
   CONSTRAINT `FK1c0y58d3n6x3m6euv2j3h64vt` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id`),
   CONSTRAINT `FKq3g33tp7xk2juh53fbw6y4y57` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -307,7 +342,7 @@ CREATE TABLE `produto_categoria` (
 
 LOCK TABLES `produto_categoria` WRITE;
 /*!40000 ALTER TABLE `produto_categoria` DISABLE KEYS */;
-INSERT INTO `produto_categoria` VALUES (1,4),(2,4),(3,5),(4,4);
+INSERT INTO `produto_categoria` VALUES (1,4),(2,4),(3,5),(4,4),(5,3),(5,4),(6,5),(7,5),(8,5),(9,5),(10,5),(11,5),(12,5),(13,5),(14,5),(15,5),(16,5),(17,5),(18,5),(19,5),(20,5),(21,5),(22,5),(23,5),(24,5),(25,5),(26,5),(27,1),(28,1),(29,1);
 /*!40000 ALTER TABLE `produto_categoria` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -320,11 +355,11 @@ DROP TABLE IF EXISTS `registro`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `registro` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `tipo_usuario` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_svqlqo2hd1f1kxd6yyi2x9dtq` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -333,35 +368,8 @@ CREATE TABLE `registro` (
 
 LOCK TABLES `registro` WRITE;
 /*!40000 ALTER TABLE `registro` DISABLE KEYS */;
-INSERT INTO `registro` VALUES (1,'lararamos@email.com','1234',1),(2,'luanalobato@email.com','1234',1),(3,'pamelalorema@email.com','1234',1),(4,'9000andre@gmail.com','admin',2);
+INSERT INTO `registro` VALUES (1,'andreraimundoo@hotmail.com','$2a$10$TFQO4.aPln9m3xz2jnxYReqRY9K1epQIPtHzVQdNmdk1arQVy.Jju'),(2,'luanalobato@gmail.com','$2a$10$ddSh0XY/lyMoilHsx1mCWeRU080z68C9A/CpXkB26iHtx/HY99EcC'),(3,'pamelalorena@gmail.com','$2a$10$25v.Uputl.in4DwLFbozFOIILnW/7LYQ3fD9HvSMoiNMroveispym');
 /*!40000 ALTER TABLE `registro` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `registro_cliente`
---
-
-DROP TABLE IF EXISTS `registro_cliente`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `registro_cliente` (
-  `registro_id` int DEFAULT NULL,
-  `cliente_id` int NOT NULL,
-  PRIMARY KEY (`cliente_id`),
-  KEY `FKogwa3ogiqmcywos7qxffwutip` (`registro_id`),
-  CONSTRAINT `FK5909cpswtbef96hdt0kn4wdbf` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`),
-  CONSTRAINT `FKogwa3ogiqmcywos7qxffwutip` FOREIGN KEY (`registro_id`) REFERENCES `registro` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `registro_cliente`
---
-
-LOCK TABLES `registro_cliente` WRITE;
-/*!40000 ALTER TABLE `registro_cliente` DISABLE KEYS */;
-INSERT INTO `registro_cliente` VALUES (1,1),(2,2),(3,3);
-/*!40000 ALTER TABLE `registro_cliente` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -373,4 +381,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-03-31 20:11:02
+-- Dump completed on 2021-05-31  9:59:08
